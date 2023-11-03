84460071d257135fb0835d69d14e231f83b4c997
Harshada0611 <harshadaa1997@gmail.com>
2023-11-03 15:08:33 +0530

    alter driver account

--- server/model/driver/account.js
+++ server/model/driver/account.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 const riderSchema = new mongoose.Schema({
-    name: { type: String },
-    email: { type: String },
-    conatct: { type: Number },
-    password: { type: String },
-    locality: { type: String },
-    city: { type: String },
-    state: { type: String },
-    pincode: { type: String },
-    profileImgUrl: {
-        type: String,
-        default:
-            "https://static.vecteezy.com/system/resources/previews/008/442/086/non_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg",
-    },
+  name: { type: String, default: "" },
+  email: { type: String },
+  contact: { type: Number },
+  password: { type: String },
+  locality: { type: String, default: "" },
+  city: { type: String, default: "" },
+  state: { type: String, default: "" },
+  pincode: { type: Number, default: "" },
+  profileImgUrl: {
+    type: String,
+    default:
+      "https://static.vecteezy.com/system/resources/previews/008/442/086/non_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg",
+  },
 });
 
 const riderModel = mongoose.model("rider", riderSchema);
--- server/route/driver/account.js
+++ server/route/driver/account.js
@@ -4,91 +4,181 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const Rider = require("../../model/driver/account");
 const jwt = require("jsonwebtoken");
+const accountMiddleware = require("../../middleware/account");
 
 // Rider Singup
-router.post("/rider-singup", async (req, resp) => {
-    const {
-        name,
+// router.post("/rider-signup", async (req, resp) => {
+//     const {
+//         name,
+//         email,
+//         contact,
+//         password,
+//         locality,
+//         city,
+//         state,
+//         pincode,
+//         profileImgUrl,
+//     } = req.body;
+//     try {
+//         const findRider = await Rider.findOne({ $or: [{ email }, { contact }] });
+//         if (findRider) {
+//             resp.json({
+//                 success: false,
+//                 msg: "Account already exists",
+//             });
+//         }
+//         if (!findRider) {
+//             const hashedPassword = await bcrypt.hash(password, 10);
+//             const createRider = await Rider.create({
+//                 name,
+//                 email,
+//                 contact,
+//                 password: hashedPassword,
+//                 locality,
+//                 city,
+//                 state,
+//                 pincode,
+//                 profileImgUrl,
+//             });
+//             console.log(createRider)
+//             resp.json({
+//                 success: true,
+//                 msg: "Rider Created",
+//                 data: createRider,
+//             });
+//         }
+//     } catch (err) {
+//         resp.json({
+//             success: false,
+//             msg: err.message,
+//         });
+//     }
+// });
+
+// Rider Singup
+
+router.post("/rider-signup", async (req, resp) => {
+  const { email, contact, password } = req.body;
+  try {
+    const findRider = await Rider.findOne({ $or: [{ email }, { contact }] });
+    if (findRider) {
+      return resp.json({
+        success: false,
+        msg: "Account already exists",
+      });
+    }
+    if (!findRider) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const createRider = Rider.create({
         email,
         contact,
-        password,
-        locality,
-        city,
-        state,
-        pincode,
-        profileImgUrl,
-    } = req.body;
-    try {
-        const findRider = await Rider.findOne({ email, contact });
-        if (findRider) {
-            resp.json({
-                success: false,
-                msg: "Rider already exists",
-            });
-        }
-        if (!findRider) {
-            const hashedPassword = await bcrypt.hash(password, 10);
-            const createRider = Rider.create({
-                name,
-                email,
-                contact,
-                password: hashedPassword,
-                locality,
-                city,
-                state,
-                pincode,
-                profileImgUrl,
-            });
-            resp.json({
-                success: true,
-                msg: "Rider Created",
-                data: createRider,
-            });
-        }
-    } catch (err) {
-        resp.json({
-            success: false,
-            msg: err.message,
-        });
+        password: hashedPassword,
+      });
+      resp.json({
+        success: true,
+        msg: "Rider Created",
+        data: createRider,
+      });
     }
+  } catch (err) {
+    resp.json({
+      success: false,
+      msg: err.message,
+    });
+  }
 });
 
 // Rider Login
 router.post("/rider-login", async (req, resp) => {
-    const { email, password } = req.body;
-    try {
-        const rider = await Rider.findOne({ email });
-        if (!rider) {
-            return resp.json({
-                success: false,
-                msg: "Rider not found",
-            });
-        }
-        const validPassword = await bcrypt.compare(password, rider.password);
-        if (!validPassword) {
-            return resp.json({
-                success: false,
-                msg: "Incorrect Credentials",
-            });
-        }
-        const token = jwt.sign(
-            { riderId: rider._id },
-            process.env.JWT_SECRET_KEY,
-            {
-                expiresIn: "1d",
-            }
-        );
-        resp.json({
-            success: true,
-            msg: "Login successful",
-            token: token,
-        });
-    } catch (err) {
-        resp.json({
-            success: false,
-            msg: err.message,
-        });
+  const { email, password } = req.body;
+  try {
+    const findRider = await Rider.findOne({ $or: [{ email }, { contact }] });
+    if (!findRider) {
+      return resp.json({
+        success: false,
+        msg: "Rider not found",
+      });
     }
+    const validPassword = await bcrypt.compare(password, findRider.password);
+    if (!validPassword) {
+      return resp.json({
+        success: false,
+        msg: "Incorrect Credentials",
+      });
+    }
+    const token = jwt.sign({ _id: findRider._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1d",
+    });
+    resp.json({
+      success: true,
+      msg: "Login successful",
+      token: token,
+    });
+  } catch (err) {
+    resp.json({
+      success: false,
+      msg: err.message,
+    });
+  }
 });
 
+// Rider Login
+//
+
+//Get profile information
+router.get("/rider-profile", accountMiddleware, async (req, resp) => {
+  try {
+    const rider = await Rider.findOne({ _id: req.accountId });
+    if (!rider) {
+      return resp.json({
+        success: false,
+        msg: "Rider not found",
+      });
+    }
+    resp.json({
+      success: true,
+      msg: "Rider Details",
+      data: rider,
+    });
+  } catch (err) {
+    resp.json({
+      success: false,
+      msg: err.message,
+    });
+  }
+});
+
+// rider-update-profile
+router.put("/rider-update-profile", accountMiddleware, async (req, resp) => {
+  try {
+    const { name, contact, locality, city, state, pincode, profileImgUrl } =
+      req.body;
+    const riderId = req.accountId;
+    const rider = await Rider.findById(riderId);
+    if (!rider) {
+      return resp.json({
+        success: false,
+        msg: "Rider not found",
+      });
+    }
+    rider.name = name;
+    rider.contact = contact;
+    rider.locality = locality;
+    rider.city = city;
+    rider.state = state;
+    rider.pincode = pincode;
+    rider.profileImgUrl = profileImgUrl;
+    const updatedRider = await rider.save();
+    resp.json({
+      success: true,
+      msg: "Rider Details updated successfully",
+      data: rider,
+    });
+  } catch (err) {
+    resp.json({
+      success: false,
+      msg: err.message,
+    });
+  }
+});
 module.exports = router;
