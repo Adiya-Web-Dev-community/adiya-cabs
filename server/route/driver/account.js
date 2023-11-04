1e8a7062ba6998717eb301e0fb240dc55aabe3b6
Harshada0611 <harshadaa1997@gmail.com>
2023-11-04 09:53:24 +0530

    resolved issues

--- server/route/driver/account.js
+++ server/route/driver/account.js
@@ -7,86 +7,13 @@ const jwt = require("jsonwebtoken");
 const accountMiddleware = require("../../middleware/account");
 
 <<<<<<< HEAD
-=======
-<<<<<<< HEAD
 // Rider Singup
 =======
 
->>>>>>> 8954e6dd98a3528bb8df49decc68f92e98ee90a5
 // Rider Singup
 
 >>>>>>> af9d28132d146bb8f3a05f5d8ab113e9f502fd51
 router.post("/rider-signup", async (req, resp) => {
-<<<<<<< HEAD
-  const { email, contact, password } = req.body;
-  try {
-    const findRider = await Rider.findOne({ $or: [{ email }, { contact }] });
-    if (findRider) {
-      return resp.json({
-        success: false,
-        msg: "Account already exists",
-      });
-    }
-    if (!findRider) {
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const createRider = Rider.create({
-        email,
-        contact,
-        password: hashedPassword,
-      });
-      resp.json({
-        success: true,
-        msg: "Rider Created",
-        data: createRider,
-      });
-    }
-  } catch (err) {
-    resp.json({
-      success: false,
-      msg: err.message,
-    });
-  }
-});
-
-// Rider Login
-router.post("/rider-login", async (req, resp) => {
-  const { userId, password } = req.body;
-  try {
-    const findRider = await Rider.findOne({
-      $or: [{ email: userId }, { contact: userId }],
-    });
-    if (!findRider) {
-      return resp.json({
-        success: false,
-        msg: "Rider not found",
-      });
-    }
-    const validPassword = await bcrypt.compare(password, findRider.password);
-    if (!validPassword) {
-      return resp.json({
-        success: false,
-        msg: "Incorrect Credentials",
-      });
-    }
-    const token = jwt.sign({ _id: findRider._id }, process.env.JWT_SECRET_KEY, {
-      expiresIn: "1d",
-    });
-    resp.json({
-      success: true,
-      msg: "Login successful",
-      token: token,
-    });
-  } catch (err) {
-    resp.json({
-      success: false,
-      msg: err.message,
-    });
-  }
-});
-
-// Rider Login
-//
-=======
     try {
         const { email, contact, password } = req.body;
         const findRider = await Rider.findOne({
@@ -177,62 +104,61 @@ router.post("/rider-login", async (req, resp) => {
         });
     }
 });
->>>>>>> 8954e6dd98a3528bb8df49decc68f92e98ee90a5
 
 //Get profile information
 router.get("/rider-profile", accountMiddleware, async (req, resp) => {
-  try {
-    const rider = await Rider.findOne({ _id: req.accountId });
-    if (!rider) {
-      return resp.json({
-        success: false,
-        msg: "Rider not found",
-      });
+    try {
+        const rider = await Rider.findOne({ _id: req.accountId });
+        if (!rider) {
+            return resp.json({
+                success: false,
+                msg: "Rider not found",
+            });
+        }
+        resp.json({
+            success: true,
+            msg: "Rider Details ",
+            data: rider,
+        });
+    } catch (err) {
+        resp.json({
+            success: false,
+            msg: err.message,
+        });
     }
-    resp.json({
-      success: true,
-      msg: "Rider Details ",
-      data: rider,
-    });
-  } catch (err) {
-    resp.json({
-      success: false,
-      msg: err.message,
-    });
-  }
 });
 
 // rider-update-profile
 router.put("/rider-update-profile", accountMiddleware, async (req, resp) => {
-  try {
-    const { name, contact, locality, city, state, pincode, profileImgUrl } =
-      req.body;
-    const riderId = req.accountId;
-    const rider = await Rider.findById(riderId);
-    if (!rider) {
-      return resp.json({
-        success: false,
-        msg: "Rider not found",
-      });
+    try {
+        const { name, contact, locality, city, state, pincode, profileImgUrl } =
+            req.body;
+        const riderId = req.accountId;
+        const rider = await Rider.findById(riderId);
+        if (!rider) {
+            return resp.json({
+                success: false,
+                msg: "Rider not found",
+            });
+        }
+        rider.name = name;
+        rider.contact = contact;
+        rider.locality = locality;
+        rider.city = city;
+        rider.state = state;
+        rider.pincode = pincode;
+        rider.profileImgUrl = profileImgUrl;
+        const updatedRider = await rider.save();
+        resp.json({
+            success: true,
+            msg: "Rider Details updated successfully",
+            data: updatedRider,
+        });
+    } catch (err) {
+        resp.json({
+            success: false,
+            msg: err.message,
+        });
     }
-    rider.name = name;
-    rider.contact = contact;
-    rider.locality = locality;
-    rider.city = city;
-    rider.state = state;
-    rider.pincode = pincode;
-    rider.profileImgUrl = profileImgUrl;
-    const updatedRider = await rider.save();
-    resp.json({
-      success: true,
-      msg: "Rider Details updated successfully",
-      data: updatedRider,
-    });
-  } catch (err) {
-    resp.json({
-      success: false,
-      msg: err.message,
-    });
-  }
 });
 module.exports = router;
