65b994cd70bc9434c8ebf2e0fdd21f1c2e47e62d
Harshada0611 <harshadaa1997@gmail.com>
2024-02-05 16:04:37 +0530

    renatl booking list

--- server/route/admin/rental-bookings.js
+++ server/route/admin/rental-bookings.js
@@ -1,8 +1,14 @@
 const router = require('express').Router()
-const RentalBooking=require('../../model/') 
+const RentalBooking = require('../../model/')
+const rental = require('../../model/bookings/rental')
 
-router.get("/get-rental-bookings", async(req, resp)=>{
-resp.send('hi')
+router.get("/get-rental-bookings", async (req, resp) => {
+    try {
+        const allBookings = await RentalBooking.find({})
+        resp.send({ success: true, allBookings })
+    } catch (err) {
+        return resp.send({ success: false, msg: err.message })
+    }
 })
 
 module.exports = router
