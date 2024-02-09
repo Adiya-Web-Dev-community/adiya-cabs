bc61f19b50de62e7aa485c019bdd5a85fc04b4b2
Ajmalshaikh04 <ajshaikh0404@gmail.com>
2024-02-09 18:21:47 +0530

    ui: add VEHICLE form and Table

--- driver/src/components/admin/RentalBookingTable.jsx
+++ driver/src/components/admin/RentalBookingTable.jsx
@@ -29,10 +29,10 @@ const RentalBookingTable = ({ data }) => {
         <table className="border-[1px] w-full">
           <thead className="border-b bg-black text-white rounded-t-lg">
             <tr>
-              <th className="px-1 py-2 italic text-md text-left">
+              <th className="px-2 py-2 italic text-md text-left">
                 Booking Date
               </th>
-              <th className="px-1 py-2 italic text-md text-left">
+              <th className="px-2 py-2 italic text-md text-left">
                 Passenger Name
               </th>
               <th className="px-2 py-2 italic text-md text-left">
@@ -46,6 +46,9 @@ const RentalBookingTable = ({ data }) => {
               </th>
               <th className="px-2 py-2 italic text-md text-left">
                 Pickup Date
+              </th>
+              <th className="px-2 py-2 italic text-md text-left">
+                Return Date
               </th>
               <th className="px-2 py-2 italic text-md text-left">Total Days</th>
               <th className="px-2 py-2 italic text-md text-left">
@@ -65,27 +68,26 @@ const RentalBookingTable = ({ data }) => {
                   "text-sm hover:bg-gray-200"
                 }
               >
-                <td className="px-1 py-2 text-sm text-left capitalize">
+                <td className="px-2 py-2 text-sm text-left capitalize">
                   {formatDate(ele.bookingDate)}
                 </td>
-                <td className="px-1 py-2 text-sm text-left">
+                <td className="px-2 py-2 text-sm text-left">
                   {ele.passengerId.name || "---"}
                 </td>
                 <td className="px-2 py-2 text-sm text-left">
-                  {ele.passengerId.email && ele.passengerId.email.length > 25
-                    ? `${ele.passengerId.email.slice(0, 25)}...`
-                    : ele.passengerId.email || "---"}
+                  {ele.passengerId.email || "---"}
                 </td>
                 <td className="px-2 py-2 text-sm text-left">
                   {ele.pickupCity || "---"}
                 </td>
-                <td className="px-2 py-2 text-sm text-left">
-                  {ele.pickupLocation && ele.pickupLocation.length > 35
-                    ? `${ele.pickupLocation.slice(0, 35)}...`
-                    : ele.pickupLocation || "---"}
+                <td className="px-2 py-2 text-sm text-left max-w-xl">
+                  {ele.pickupLocation || "---"}
                 </td>
                 <td className="px-2 py-2 text-sm text-left">
                   {formatDate(ele.pickupDate)}
+                </td>
+                <td className="px-2 py-2 text-sm text-left">
+                  {formatDate(ele.endDate)}
                 </td>
                 <td className="px-2 py-2 text-sm text-left">
                   {ele.totalDays || "---"}
@@ -93,7 +95,7 @@ const RentalBookingTable = ({ data }) => {
                 <td className="px-2 py-2 text-sm text-left">
                   {ele.payableAmount || "---"}
                 </td>
-                <td className="px-3 py-2 text-lg text-left flex justify-center gap-5">
+                <td className="px-2 py-2 text-lg text-left flex justify-center item-center ">
                   <span
                     className="w-4 h-4 cursor-pointer inline-block"
                     onClick={() => view(ele.carId)}
