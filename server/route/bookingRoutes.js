ada04a8a75c95855bab4fb4562befe891040cce8
Ajmalshaikh04 <ajshaikh0404@gmail.com>
2024-12-30 17:55:32 +0530

    upcomming bookings

--- server/route/bookingRoutes.js
+++ server/route/bookingRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { confirmBooking } = require('../controller/bookingController');
+const { confirmBooking, getBookings } = require('../controller/bookingController');
 const accountMiddleware = require('../middleware/account');
 
 // Confirm Booking API
 router.post('/confirm', accountMiddleware, confirmBooking);
+
+// Get Bookings API
+router.get('/', accountMiddleware, getBookings);
 
 module.exports = router;
